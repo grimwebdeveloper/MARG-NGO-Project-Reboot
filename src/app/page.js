c582099d04174fbac537159344eea3f9044bca88
minhaj alam <myminhajalam392@gmail.com>
2025-08-20 12:06:51 +0530

    add slide&sponsor

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import SliderHero from '@/components/home/SliderHero';
+import Slide from '@/components/slide/Slide';
+import Sponsors from '@/components/sponsor/Sponsors';
 
 function page() {
   return (
     <main>
       <SliderHero />
+      <Slide/>
+      <Sponsors/>
     </main>
   );
 }
